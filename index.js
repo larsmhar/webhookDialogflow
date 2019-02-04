61b4e2cf5b6852897f13d3f9db7427fe6d062b27
larsmhar <lars.martin.hardeland@gmail.com>
2019-02-04 13:13:42 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const { WebhookClient } = require('dialogflow-fulfillment')
+const app = express()
+
+app.get('/', (req, res) => res.send('online'))
+app.post('/dialogflow', express.json(), (req, res) => {
+  const agent = new WebhookClient({ request: req, response: res })
+
+  function welcome() {
+    agent.add('Welcome to my agent!')
+  }
+
+  function fallback() {
+    agent.add('Sorry, can you repeat that?')
+  }
+
+  function webhookTest() {
+    agent.add('This is a webhook test of some sorts!')
+  }
+
+  function testTest() {
+    agent.add('This is a test!')
+  }
+	console.log(JSON.stringify(req.body))
+	console.log(agent.intent)
+	//console.log(req.body.queryResult.fulfillmentMessages[0].text)
+	//console.log(req.body.queryResult.intent)
+  let intentMap = new Map()
+  intentMap.set('Default Welcome Intent', welcome)
+  intentMap.set('testTest', testTest)
+  intentMap.set('webhookTest', webhookTest)
+  agent.handleRequest(intentMap)
+})
+
+app.listen(process.env.PORT || 8080)
