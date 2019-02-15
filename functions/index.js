627e291ce9fec3835b279d3989a54aaf2f6e09a0
larsmhar <lars.martin.hardeland@gmail.com>
2019-02-15 13:34:15 +0100

    Fixed firebase functions and cleaned up stuff

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,31 @@
+// Remember not to declare anything unecessary in the global scope as it eats ram
+// and can cause the function to crash
 const functions = require('firebase-functions');
-
 const admin = require('firebase-admin');
+const axios = require('axios');
+const {WebhookClient} = require('dialogflow-fulfillment');
 
-//process.env.GOOGLE_APPLICATION_CREDENTIALS = './.env/hurtighjelpern-firebase-adminsdk-n8o9s-c7918f9116.json';
+// The API credentials
+const serviceAccount = require('./.env/hurtighjelpern-firebase-adminsdk-n8o9s-c7918f9116.json');
 
-var serviceAccount = require('./.env/hurtighjelpern-firebase-adminsdk-n8o9s-c7918f9116.json');
-
-//console.log(admin.credential.cert(serviceAccount));
+// Sets the credentials required to upload new functions
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://hurtighjelpern.firebaseio.com"
 });
 
+// All of our functions go here
+exports.webhook = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+  function webhookTest(agent) {
+    const randomNum = Math.round(Math.random() * 100);
+    agent.add(`This is a webhookTest and this is a number between 0 and 100: ${randomNum}`)
+  }
 
-
-
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest((req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    return res.redirect(303, snapshot.ref.toString());
-  });
-});
+  let intentMap = new Map();
+  // Sets response functions based on intent
+  // The first argument is the intent and the second is the response
+  intentMap.set('webhookTest', webhookTest);
+  agent.handleRequest(intentMap);
+})
